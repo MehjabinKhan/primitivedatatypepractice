c30e11c2d320792bde531cc8ee9371f49dda5944
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-02 23:14:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var firstName = "Mehjabin";
+console.log(firstName);
+var isstuding = "True";
+console.log(isstuding);
+var height = 5.4;
+console.log(height);
+var myName = "mehjabin";
+var hello = "Hello + myName";
+console.log(hello);
---
+++ main.ts
@@ -0,0 +1,9 @@
+let firstName = "Mehjabin";
+console.log(firstName);
+let isstuding = "True";
+console.log(isstuding);
+let height = 5.4;
+console.log(height);
+let myName = "mehjabin";
+let hello = "Hello + myName";
+console.log(hello);
